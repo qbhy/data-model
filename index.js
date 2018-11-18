4e5410652e7c28a16bb6f21bc21646f006214a80
qbhy <572490755@qq.com>
2018-11-18 12:14:36 +0800

    fixed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import Data from 'src/data';
-import Model from 'src/model';
-import Paginate from 'src/paginate';
-import List from 'src/list';
+import Data from './src/data';
+import Model from './src/model';
+import Paginate from './src/paginate';
+import List from './src/list';
 
 export {
     Data,
